235f625ef5c892b621343d4aa6ef11bdf38aff3c
David Laignelet <laignelet.david@gmail.com>
2023-05-20 15:40:39 +0200

    Controllers
    #23
    Création des controllers MenuController.php, ReservationController.php et ContactController.php, et des vues associées.

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MenuController extends AbstractController
+{
+    #[Route('/menu', name: 'app_menu')]
+    public function show(): Response
+    {
+        return $this->render('menu/show.html.twig', [
+            'controller_name' => 'MenuController',
+        ]);
+    }
+}
